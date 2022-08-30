7cfe82050e39ff73a5e58cf46bb4945c10f5b1c0
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-08-30 20:55:27 +0200

    the app without localStorage

--- app.js
+++ app.js
@@ -3,14 +3,55 @@ const input = document.querySelector('.input');
 const tasksContainer = document.querySelector('.tasks');
 const btnDel = document.querySelector('.del');
 const tasks = [];
+const state ={
+    task:{}
+}
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
     const value = input.value;
     if(value ==='') return;
-    console.log('testtt')
-    const mark = `<div class="task">${value} <span class="del">Delete</span></div>`;
-    tasksContainer.insertAdjacentHTML('beforeend',mark);
-    input.value ='';
-
+    state.task = {
+        id:Date.now(),
+        title:value,
+        complete: false,
+    }
+    tasks.push(state.task);
+    console.log(state.task);
+    console.log(tasks);
+    renderTasks(tasks);
+    
 })
+const renderTasks = function (arrayTasks){
+    let mark;
+    tasksContainer.innerHTML = '';
+    arrayTasks.forEach(task => {
+        if(task.complete){
+            mark = `<div class="task done" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
+        } else {
+             mark = `<div class="task" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
+        }
+        tasksContainer.insertAdjacentHTML('afterbegin',mark);
+    });
+    input.value ='';
+}
+
+tasksContainer.addEventListener('click', (e) => {
+    const click = e.target.closest('.del');
+    const done = e.target.closest('.task') ;
+    if(done){
+        done.classList.toggle('done');
+    }
+    if(click) {
+        console.log('del');
+        const div = click.closest('.task')
+        const idDiv = div.dataset.id;
+        const index = tasks.findIndex(task => task.id===+idDiv);
+        tasks.splice(index, 1)
+        renderTasks(tasks);       
+    }
+})
+
+const updateTask = function(div) {
+
+}
