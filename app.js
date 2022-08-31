aa4d0501fda2aa702977c93295e9699a30449d78
ahmedsamir122 <eng.ahmed.elmitwalli@gmail.com>
2022-08-31 04:59:09 +0200

    add localstorage

--- app.js
+++ app.js
@@ -2,10 +2,38 @@ const btn = document.querySelector('.btn');
 const input = document.querySelector('.input');
 const tasksContainer = document.querySelector('.tasks');
 const btnDel = document.querySelector('.del');
-const tasks = [];
+let tasks = [];
 const state ={
     task:{}
 }
+
+// const getDataLocalStorage = function() {
+//     let data = window.localStorage.getItem('tasks');
+//     if(data) {
+//         let tasks = JSON.parse(data);
+//     }
+// }
+
+if(localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+}
+
+const renderTasks = function (arrayTasks){
+    let mark;
+    tasksContainer.innerHTML = '';
+    arrayTasks.forEach(task => {
+        if(task.complete){
+            mark = `<div class="task done" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
+        } else {
+             mark = `<div class="task" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
+        }
+        tasksContainer.insertAdjacentHTML('afterbegin',mark);
+    });
+    input.value ='';
+}
+// getDataLocalStorage();
+renderTasks(tasks);
+
 
 btn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -20,31 +48,22 @@ btn.addEventListener('click', (e) => {
     console.log(state.task);
     console.log(tasks);
     renderTasks(tasks);
-    
+    addToLocalStorage(tasks);
 })
-const renderTasks = function (arrayTasks){
-    let mark;
-    tasksContainer.innerHTML = '';
-    arrayTasks.forEach(task => {
-        if(task.complete){
-            mark = `<div class="task done" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
-        } else {
-             mark = `<div class="task" data-id ="${task.id}">${task.title} <span class="del">Delete</span></div>`;
-        }
-        tasksContainer.insertAdjacentHTML('afterbegin',mark);
-    });
-    input.value ='';
-}
+
 
 tasksContainer.addEventListener('click', (e) => {
     const click = e.target.closest('.del');
     const done = e.target.closest('.task') ;
-    
+
     updateTask(done);
 
     deleteTask(click);
 
+    addToLocalStorage(tasks);
+
     console.log(tasks);
+
 })
 
 const updateTask = function(div) {
@@ -71,3 +90,8 @@ const deleteTask =function(element){
         renderTasks(tasks);       
     }
 }
+
+const addToLocalStorage = function(array){
+    window.localStorage.setItem('tasks', JSON.stringify(array));
+}
+
